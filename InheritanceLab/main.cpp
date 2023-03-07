aa132a306de87c0c44ed21d978da17d866da41b6
Refai <mohamedref11@gmail.com>
2023-03-07 15:55:02 +0200

    Initial Commit

---
+++ InheritanceLab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  InheritanceLab
+//
+//  Created by Mohamed El-Refai on 07/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
